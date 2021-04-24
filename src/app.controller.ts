dfa82f264028c3eb95a9f0088d5495fb757dc212
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2021-04-24 13:45:37 +0200

    feat(controller): extends app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): Promise<unknown> {
-    return this.appService.getHello();
+  @Get('/clear-cache')
+  clearCache(): Promise<unknown> {
+    return this.appService.clearCache();
+  }
+
+  @Get('/item/:id')
+  findOne(@Param() params): Promise<unknown> {
+    return this.appService.get(params.id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
 import { RedisCacheService } from './services/cache-service/redis-cache.service';
+import { randomValue } from './utils/utils';
 
 @Injectable()
 export class AppService {
   constructor(private readonly cache: RedisCacheService) {}
 
-  getHello(): Promise<unknown> {
+  clearCache(): Promise<unknown> {
     return this.cache.clearCache();
+  }
+
+  get(key: string): Promise<unknown> {
+    return this.cache.get(key, randomValue);
+  }
+
+  delete(key: string): Promise<unknown> {
+    return this.cache.delete(key);
+  }
+
+  exists(key: string): Promise<unknown> {
+    return this.cache.exists(key);
   }
 }
